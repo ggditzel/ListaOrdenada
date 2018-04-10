0b61352df38b86781ec1ef22b598de8b06de2287
ggditzel@gmail.com <ggditzel@gmail.com>
2018-04-10 02:30:08 -0300

    Aprimoramentos gerais
    Métodos que não estão funcionando (ou precisam ser implementados para
    que outros funcionem):
     - inserirApos();
     - inserirFinal();
    
    E outros que o professor passou (ex: lerPosAtual())

--- ListaOrdenada/src/listaDupla/ListaEncadeada.java
+++ ListaOrdenada/src/listaDupla/ListaEncadeada.java
@@ -6,73 +6,28 @@ public class ListaEncadeada<T extends IBuscavel> {
 
 	private Caixa<T> novo;
 	private Caixa<T> primeiro;
-	//private Caixa<T> ultimo;
 	private Caixa<T> cursor;
-
-	//	public Caixa<T> novo;
-	//	public Caixa<T> primeiro;
-	//	public Caixa<T> ultimo;
-	//	public Caixa<T> cursor;
-
-
 	private int size;
 
 	public ListaEncadeada(){
 		novo = null;
 		primeiro = null;
-		//ultimo = null;
 		cursor = null;
 		size = 0;
 	}
 
 	public void inserirFinal(T valor) {
-		novo = new Caixa<T>(valor); // assumindo memória infinita, etc
-		size++;
-		if (primeiro == null) {
-			primeiro = cursor = novo;
-			primeiro.setAnterior(primeiro);
-			primeiro.setProximo(primeiro);
-		} else {
-
-			novo.setProximo(primeiro); // circular
-			novo.setAnterior(primeiro.getAnterior());
-			primeiro.getAnterior().setProximo(novo);
-			primeiro.setAnterior(novo);
-
-
-			//			ultimo.setProximo(novo);
-			//			novo.setAnterior(ultimo);
-			//			novo.setProximo(primeiro); // circular
-			//			ultimo = novo;
-		}
-		System.out.printf("Elmento inserido no final: %s; primeiro: %s; cursor: %s\n", novo, primeiro, cursor);
+		cursor = primeiro.getAnterior(); // ultimo
+		
+		// implementar metodo inserirApos(), para usa-lo aqui;
+		
+		
 	}
 
 	public void inserirInicio(T valor) {
 		cursor = primeiro;
-		inserirPosAtual(valor);
-//		novo = new Caixa<T>(valor);
-//		size++;
-//		if (primeiro == null) {
-//			primeiro = cursor = novo;
-//			primeiro.setAnterior(primeiro);
-//			primeiro.setProximo(primeiro);
-//		} else {
-//			Caixa<T> ultimo = primeiro.getAnterior();
-//			novo.setAnterior(ultimo);
-//			novo.setProximo(primeiro);
-//			ultimo.setProximo(novo);
-//			primeiro.setAnterior(novo);
-//			primeiro = novo;
-
-
-
-			//			primeiro.setAnterior(novo);
-			//			novo.setProximo(primeiro);
-			//			novo.setAnterior(ultimo);
-			//			primeiro = novo;
-		}
-		System.out.printf("Elmento inserido no inicio %s; primeiro: %s; cursor: %s\n", novo, primeiro, cursor);
+		inserirPosAtual(valor); // equivalente a "insere antes" (da forma que foi implementado)
+		//System.out.printf("Elemento inserido no inicio: %s; primeiro: %s; cursor: %s\n", novo, primeiro, cursor);
 	}
 
 	public T retirarInicio() throws Exception {
@@ -80,7 +35,7 @@ public class ListaEncadeada<T extends IBuscavel> {
 		if (primeiro == null) throw new Exception("Fila vazia");
 
 		T elemento = primeiro.getElemento();
-		excluirInicio();    	
+		excluirInicio();
 		return elemento;
 	}
 
@@ -89,6 +44,7 @@ public class ListaEncadeada<T extends IBuscavel> {
 		if (primeiro == null) throw new Exception("Fila vazia");
 
 		T elemento = primeiro.getAnterior().getElemento();
+		//System.out.println("Endereco do elemento a ser retirado do final: " + primeiro.getAnterior());
 		excluirFinal();
 
 		return elemento;
@@ -102,146 +58,107 @@ public class ListaEncadeada<T extends IBuscavel> {
 		return size;
 	}
 
-	// PRECISA FAZER A BUSCA A PARTIR DA POSICAO DO CURSOR, E CIRCULAR
-	//    public Caixa<T> buscar(int codigo) throws Exception {
-	//    	if (primeiro == null) throw new Exception("Fila vazia");
-	//    	
-	//    	while (cursor.getElemento().getCodigo() != codigo) {
-	//    		if (cursor != ultimo) {
-	//    			cursor = cursor.getProximo();
-	//    		} else throw new Exception("Elemento nao encontrado.");
-	//    	}
-	//    	return cursor;
-	//    }
-	//    
-	//    public Caixa<T> buscar2(T elemento) throws Exception {
-	//    	if (primeiro == null) throw new Exception("Fila vazia");
-	//    	    	
-	//    	inserirFinal(elemento); // fake
-	//    	cursor = primeiro; // mais fácil procurar sempre a partir do primeiro
-	//    	
-	//    	while (cursor.getElemento() != elemento) {
-	//    		cursor = cursor.getProximo();
-	//    	}
-	//    	
-	//    	if (cursor != ultimo) {
-	//    		excluirFinal();
-	//    		return cursor;
-	//    	} else {
-	//    		excluirFinal();
-	//    		throw new Exception("Elemento nao encontrado no buscar2");
-	//    	}
-	//    }
-
-	public Caixa<T> buscar3(T elemento) throws Exception {
+	// apenas para posicionar o cursor, para eventual leitura/escrita;
+	public void buscar(T elemento) throws Exception {
 		if (primeiro == null) throw new Exception("Fila vazia");
+		
+		if (cursor.getElemento() != elemento) { // caso ja seja o elemento, nao faz nada
 
-		Caixa<T> posicaoInicial = cursor;
-		Caixa<T> posicaoBusca = cursor;
-		inserirPosAtual(elemento); // fake
-		avancarCursor();
-		System.out.println("Elemento fake inserido na posicao: " + cursor);
-		while (cursor.getElemento() != elemento) {
-			System.out.println("Cursor vai para proxima posicao: " + cursor.getProximo());
-			cursor = cursor.getProximo();
-		}
+			System.out.println("\n=== busca elemento ===");
+			//System.out.println("Posicao atual do cursor: " + cursor);
+			//Caixa<T> posicaoFake = cursor; //.getAnterior(); // posicao onde o elemento fake sera inserido
+			inserirPosAtual(elemento); // fake
+			Caixa<T> posicaoFake = cursor.getAnterior(); // posicao do fake;
+			//avancarCursor();
+			System.out.println("Elemento fake inserido na posicao: " + posicaoFake);
+			while (cursor.getElemento() != elemento) {
+				//System.out.println("Cursor vai para proxima posicao: " + cursor.getProximo());
+				cursor = cursor.getProximo();
+			}
 
-		if (cursor != posicaoInicial) {
-			posicaoBusca = cursor;
-			System.out.println("Posicao onde elemento foi encontrado: " + cursor);
-			cursor = posicaoInicial; // preparar para exclusao
-			excluirPosAtual();
-			System.out.println("Posicao do cursor apos exclusao do fake: " + cursor);
-			return posicaoBusca;
-		} else {
-			excluirPosAtual();
-			throw new Exception("Elemento nao encontrado no buscar3");
+			if (cursor != posicaoFake) {
+				Caixa<T> posicaoEncontrado = cursor;
+				System.out.println("Posicao onde elemento foi encontrado: " + cursor);
+				cursor = posicaoFake; // preparar para exclusao do fake 
+				excluirPosAtual();
+				cursor = posicaoEncontrado;
+				System.out.println("posicao do cursor ao deixar a busca: " + cursor);
+				System.out.println("=== fim busca elemento ===\n");
+			} else {
+				excluirPosAtual();
+				throw new Exception("Elemento nao encontrado");
+			}
 		}
+		
 	}
 
 	public void excluirInicio() throws Exception {
 		cursor = primeiro;
 		excluirPosAtual();
-//		if (primeiro == null) throw new Exception("Fila vazia");
-//
-//		size--;
-//
-//		Caixa<T> ultimo = primeiro.getAnterior();
-//		primeiro.getProximo().setAnterior(ultimo);
-//		primeiro = primeiro.getProximo();
-//		ultimo.setProximo(primeiro);
-//		cursor.setProximo(primeiro); // aponta para o elemento posterior ao excluido
-		
-		System.out.printf("Elmento excluido do inicio da lista; primeiro: %s; cursor: %s\n", primeiro, cursor);
-
-		//    	if (primeiro == ultimo) {		// só resta um elemento na fila
-		//    		primeiro = ultimo = cursor = null;	// reset
-		//    	} else {
-		//    		primeiro = primeiro.getProximo();
-		//    		primeiro.setAnterior(null); // o novo "primeiro" não aponta para nenhum anterior
-		//    	}
+		//System.out.printf("Elmento excluido do inicio da lista; primeiro: %s; cursor: %s\n", primeiro, cursor);
 	}
 
 	public void excluirFinal() throws Exception {
 		cursor = primeiro.getAnterior();
+		System.out.println("Elemento a ser excluido do final: " + cursor);
 		excluirPosAtual();
-		
-		
-//		if (primeiro == null) throw new Exception("Fila vazia");
-//
-//		if (size == 1) {					// só resta um elemento na fila
-//			primeiro = cursor = null;		// reset
-//		} else {
-//
-//			Caixa<T> ultimo = primeiro.getAnterior();
-//			primeiro.setAnterior(ultimo.getAnterior());
-//			ultimo.getAnterior().setProximo(primeiro);
-//			cursor.setProximo(primeiro); // aponta para o elemento posterior ao excluido
-//		}
-//		size--;
-
 	}
 
-
 	public void excluirPosAtual() throws Exception {
+		System.out.println("\nPosicao do cursor ao chegar no metodo para excluir atual: " + cursor);
 		if (primeiro == null) throw new Exception("Fila vazia");
 
-		if (size == 1) {					// só resta um elemento na fila
-			primeiro = cursor = null;		// reset
+		if (size == 1) {				// ultimo elemento
+			primeiro = cursor = null;	// apaga tudo, reset
 		} else {
+			
+			if (cursor == primeiro) {
+				primeiro = primeiro.getProximo();	// se era a primeira posicao, atualiza
+			}
+			
 			cursor.getAnterior().setProximo(cursor.getProximo());
 			cursor.getProximo().setAnterior(cursor.getAnterior());
 			cursor = cursor.getProximo(); // aponta para o elemento posterior ao excluido
+			//System.out.println("Posicao do cursor apos exclusao atual: \n" + cursor);
 		}
+
 		size--;
 	}
 
+	// vai ter que tomar cuidado quando so tiver um elemento
 	public void excluirItem(T elemento) throws Exception {
-		buscar3(elemento); //posiciona o cursor
+		buscar(elemento); //posiciona o cursor
 		excluirPosAtual();
 	}
 
 	public void inserirPosAtual(T elemento) {
 		novo = new Caixa<T>(elemento); // assumindo memória infinita, etc
-		size++;
 		if (primeiro == null) {
 			primeiro = cursor = novo;
 			primeiro.setAnterior(primeiro); // se so existe o primeiro elemento, ele aponta para ele mesmo
 			primeiro.setProximo(primeiro);
+			System.out.printf("Endereco primeiro elemento: %s\n", novo);
 
 		} else {
+			
+			if (cursor == primeiro & size >= 1) {
+				primeiro = novo;
+			}
+			
+			System.out.println("Inserido novo elemento na posicao: " + novo);
 			novo.setAnterior(cursor.getAnterior());
 			novo.setProximo(cursor);
 			cursor.getAnterior().setProximo(novo);
 			cursor.setAnterior(novo);
 			// cursor permanece igual, ou seja, aponta para o elemento apos o incluido
-			System.out.println("Endereco cursor apos inserir posicao atual: " + cursor);
+			System.out.println("Endereco cursor apos insercao " + cursor);
 		}
+		size++;
 
 	}
 
 	public void avancarCursor() {
-		System.out.printf("Primeiro: %s; Posicao atual cursor: %s; proxima posicao cursor: %s\n", primeiro, cursor, cursor.getProximo());
+		System.out.printf("Cursor avancou de %s para %s\n", cursor, cursor.getProximo());
 		cursor = cursor.getProximo();
 	}
 
--- ListaOrdenada/src/listaDupla/Testes.java
+++ ListaOrdenada/src/listaDupla/Testes.java
@@ -2,29 +2,27 @@ package listaDupla;
 
 public class Testes {
 
-public static void main(String[] args) {
-	
-	Aluno buscado;
-		
+	public static void main(String[] args) {
+
 		ListaEncadeada<Aluno> l1 = new ListaEncadeada<Aluno>();
-		
+
 		System.out.printf("Fila vazia? %s\n", l1.isEmpty() ? "Sim" : "Nao");
-		
+
 		Aluno a1 = null;
 		Aluno a2 = null;
 		Aluno a3 = null;
 		Aluno a4 = null;
-		Aluno desconhecido = null;
+		Aluno a5 = null;
 		try {
 			a1 = new Aluno("Jose");
 			a2 = new Aluno("Joao");
 			a3 = new Aluno("Silvio");
 			a4 = new Aluno("Paulo");
-			desconhecido = new Aluno("Ninguem");
-//			l1.inserirFinal(a4);
-//			l1.inserirFinal(a1);
-//			l1.inserirFinal(a3);
-//			l1.inserirFinal(a2);
+			a5 = new Aluno("Ninguem");
+//			 l1.inserirFinal(a4);
+//			 l1.inserirFinal(a1);
+//			 l1.inserirFinal(a3);
+//			 l1.inserirFinal(a2);
 			l1.inserirInicio(a4);
 			l1.inserirInicio(a1);
 			l1.inserirInicio(a3);
@@ -32,70 +30,73 @@ public static void main(String[] args) {
 			for (int i = 1; i <= 3; i++) {
 				l1.avancarCursor();
 			}
-			l1.inserirPosAtual(desconhecido);
-			
+			l1.inserirPosAtual(a5);
+
 		} catch (Exception e2) {
 			System.out.println(e2);
 		}
-		
+
 		System.out.printf("Fila vazia? %s\n", l1.isEmpty() ? "Sim" : "Nao");
-		
-//		try {
-//			buscado = l1.buscar(1).getElemento();
-//			System.out.printf("Aluno encontrado pelo codigo: %s; matricula: %d; cursor: %s\n", buscado.getNome(), buscado.getCodigo(), buscado);
-//		} catch (Exception e1) {
-//			System.out.println(e1);
-//		}
-//		
-//		try {
-//			buscado = l1.buscar2(a3).getElemento();
-//			System.out.printf("Aluno encontrado: %s; matricula: %d; cursor: %s\n", buscado.getNome(), buscado.getCodigo(), buscado);
-//		} catch (Exception e1) {
-//			System.out.println(e1);
-//		}
-//		
-//		try {
-//			buscado = l1.buscar2(desconhecido).getElemento();
-//			System.out.printf("Aluno encontrado: %s; matricula: %d; cursor: %s\n", buscado.getNome(), buscado.getCodigo(), buscado);
-//		} catch (Exception e1) {
-//			System.out.println(e1);
-//		}
 
-//		try {
-//		l1.excluirItem(a3);
-//	} catch (Exception e1) {
-//		System.out.println(e1);
-//	}
-		
-		try {
-			buscado = l1.buscar3(a3).getElemento();
-			System.out.printf("Aluno encontrado: %s; matricula: %d; cursor: %s\n", buscado.getNome(), buscado.getCodigo(), buscado);
-		} catch (Exception e1) {
-			System.out.println(e1);
-		}
+		// try {
+		// buscado = l1.buscar(1).getElemento();
+		// System.out.printf("Aluno encontrado pelo codigo: %s; matricula: %d; cursor:
+		// %s\n", buscado.getNome(), buscado.getCodigo(), buscado);
+		// } catch (Exception e1) {
+		// System.out.println(e1);
+		// }
+		//
+		// try {
+		// buscado = l1.buscar2(a3).getElemento();
+		// System.out.printf("Aluno encontrado: %s; matricula: %d; cursor: %s\n",
+		// buscado.getNome(), buscado.getCodigo(), buscado);
+		// } catch (Exception e1) {
+		// System.out.println(e1);
+		// }
+		//
+		// try {
+		// buscado = l1.buscar2(desconhecido).getElemento();
+		// System.out.printf("Aluno encontrado: %s; matricula: %d; cursor: %s\n",
+		// buscado.getNome(), buscado.getCodigo(), buscado);
+		// } catch (Exception e1) {
+		// System.out.println(e1);
+		// }
 
-//		l1.avancarCursor();
-//		l1.avancarCursor();
-//		l1.avancarCursor();
+		// try {
+		// l1.excluirItem(a3);
+		// } catch (Exception e1) {
+		// System.out.println(e1);
+		// }
+
 		try {
+			l1.buscar(a5);
 			l1.excluirPosAtual();
 		} catch (Exception e1) {
 			System.out.println(e1);
 		}
 
-	
+		// l1.avancarCursor();
+		// l1.avancarCursor();
+		// l1.avancarCursor();
+//		try {
+//			l1.excluirPosAtual();
+//		} catch (Exception e1) {
+//			System.out.println(e1);
+//		}
+
 		try {
-			for (int i = 1; i <= 5; i++) {
+			for (int i = 1; i <= 4; i++) {
 				//Aluno a = l1.retirarInicio();
 				Aluno a = l1.retirarFinal();
-				System.out.printf("Aluno retirado da lista: %s; matricula: %d; referencia: %s\n", a.getNome(), a.getCodigo(), a);
+				System.out.printf("Aluno retirado da lista: %s; matricula: %d; referencia: %s\n", a.getNome(),
+						a.getCodigo(), a);
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		System.out.printf("Fila vazia? %s\n", l1.isEmpty() ? "Sim" : "Nao");
-		
+
 	}
-	
+
 }
